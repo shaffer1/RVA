481c63774afe6fc6210b86b9ccb34bfef12ceead
Mark Van Moer <mvanmoer@illinois.edu>
2014-12-10 16:02:27 -0600

    Initial commit of header.
    Just have a slight core of methods. These might more properly be
    implemented as helper functions.

---
+++ EclipseASCIIReader/EclipseASCIIReader.h
@@ -0,0 +1,40 @@
+// Eclipse ASCII grdecl reader. 20141210
+// Mark Van Moer, NCSA
+
+#ifndef __EclipseASCIIReader_h
+#define __EclipseASCIIReader_h
+
+#include "vtkUnstructuredGridAlgorithm.h"
+#include "vtkPoints.h"
+#include "vtkSmartPointer.h"
+#include <string>
+
+class VTK_IO_EXPORT EclipseASCIIReader : public vtkUnstructuredGridAlgorithm
+{
+public:
+    static EclipseASCIIReader *New();
+    vtkTypeMacro(EclipseASCIIReader, vtkUnstructuredGridAlgorithm);
+    void PrintSelf(ostream& os, vtkIndent indent);
+
+    vtkSetStringMacro(FileName);
+    vtkGetStringMacro(FileName);
+
+protected:
+    EclipseASCIIReader();
+    ~EclipseASCIIReader();
+
+    char* FileName;
+
+    // MVM: note to self -- based on my eclipse2vtk.py script
+    void ConvertGrid(std::string, vtkUnstructuredGrid*);
+    void CreateVTKCells(vtkUnstructuredGrid*, int, int, int);
+    vtkPoints* CreateVTKPoints(int, int, int);
+
+    virtual int RequestData(vtkInformation*, vtkInformationVector**, vtkInformationVector*);
+    virtual int RequestInformation(vtkInformation*, vtkInformationVector**, vtkInformationVector*);
+
+private:
+    EclipseASCIIReader(const EclipseASCIIReader&); // Not implemented.
+    void operator=(const EclipseASCIIReader&); // Not implemented.
+};
+#endif
