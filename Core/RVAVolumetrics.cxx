006d1e287a0fefb0f383613e5f9a445b84cd02e2
Mark Van Moer <mvanmoer@illinois.edu>
2015-01-16 16:33:45 -0600

    Minimum integrate pass-through
    Passes a dummy value through integration. This was just to get
    everything working because analysis filters like vtkIntegrateAttributes
    are a little different to handle. A single vertex/point is created to
    attached the result to, instead of passing through the entire dataset.
    In that sense, this is a terminal filter.

--- Core/RVAVolumetrics.cxx
+++ Core/RVAVolumetrics.cxx
@@ -9,7 +9,8 @@
 #include "vtkUnstructuredGrid.h"
 #include "vtkCellData.h"
 #include "vtkDoubleArray.h"
-
+#include "vtkVertex.h"
+#include "vtkPoints.h"
 
 vtkStandardNewMacro(RVAVolumetrics);
 
@@ -51,7 +52,7 @@ int RVAVolumetrics::RequestData(vtkInformation *vtkNotUsed(request),
     vtkInformation *outInfo = outputVector->GetInformationObject(0);
 
     vtkDataSet *input = vtkDataSet::SafeDownCast(inInfo->Get(vtkDataObject::DATA_OBJECT()));
-    vtkDataSet *output = vtkDataSet::SafeDownCast(outInfo->Get(vtkDataObject::DATA_OBJECT()));
+    vtkUnstructuredGrid *output = vtkUnstructuredGrid::SafeDownCast(outInfo->Get(vtkDataObject::DATA_OBJECT()));
 
     if (!output)
     {
@@ -68,8 +69,27 @@ int RVAVolumetrics::RequestData(vtkInformation *vtkNotUsed(request),
     // Step 3. user selectable arrays.
     this->integrate->SetInput(input);
     this->integrate->Update();
-    
-    output->ShallowCopy(input);
+    vtkStdString scalarName;
+    scalarName = this->integrate->GetOutput()->GetCellData()->GetArrayName(0);
+    this->integrate->GetOutput()->GetCellData()->SetActiveScalars(scalarName.c_str());
+
+    double dummy = 0.12345;
+
+    // The Integrate Variables filter creates a data set of a single point on which
+    // to attached the field data output. This does the same.
+    vtkSmartPointer<vtkPoints> pt = vtkSmartPointer<vtkPoints>::New();
+    pt->InsertNextPoint(0.0, 0.0, 0.0);
+    vtkSmartPointer<vtkVertex> vtx = vtkSmartPointer<vtkVertex>::New();
+    vtx->GetPointIds()->InsertNextId(0);
+    vtkSmartPointer<vtkDoubleArray> results = vtkSmartPointer<vtkDoubleArray>::New();
+    results->SetName("Results");
+    results->InsertNextTuple(&dummy);
+   
+    output->Allocate(1);
+    output->InsertNextCell(vtx->GetCellType(), vtx->GetPointIds());
+    output->SetPoints(pt);
+    output->GetCellData()->SetScalars(results);
+
     return 1;
 }
 
